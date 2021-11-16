b7e3c52fecfc04e2cdbe45ead9a73016c9bd052d
Yuqiao Wen <yuqiao@ualbeta.ca>
2021-11-16 15:37:06 +0800

    Implement least-overlap

--- preprocessing/dd/clean_dialogs.py
+++ preprocessing/dd/clean_dialogs.py
@@ -26,7 +26,6 @@ def get_dialogs(path):
     dialogs = []
     with open(path, mode='r') as f:
         for line in f:
-            line = line.replace('__eou__', '')
             dialogs.append(line)
     return dialogs
 
@@ -38,6 +37,7 @@ def build_w2i(dialogs):
     # Build the counter
     counter = Counter()
     for dialog in dialogs:
+        dialog = dialog.replace('__eou__', '')
         grams = preprocess_utils.get_grams(dialog, n=1)
         for gram in grams:
             counter[gram] += 1
@@ -62,6 +62,7 @@ def build_bow(dialogs, w2i):
     bow = torch.zeros((num_examples, vocab_size), dtype=torch.float16).cuda()
 
     for idx, dialog in enumerate(dialogs):
+        dialog = dialog.replace('__eou__', '')
         grams = preprocess_utils.get_grams(dialog, n=1)
         for gram in grams:
             bow[idx, w2i[gram]] = 1
@@ -70,6 +71,7 @@ def build_bow(dialogs, w2i):
 TRAIN_PATH = '../../data/ijcnlp_dailydialog/train/dialogues_train.txt'
 VALID_PATH = '../../data/ijcnlp_dailydialog/validation/dialogues_validation.txt'
 TEST_PATH = '../../data/ijcnlp_dailydialog/test/dialogues_test.txt'
+FULL_PATH = '../../data/ijcnlp_dailydialog/dialogues_text.txt'
 
 if __name__ == '__main__':
 
@@ -132,4 +134,46 @@ if __name__ == '__main__':
 
     elif args.mode == 'split':
 
-        print('splitting!')
+        NUM_TEST = 1000
+        NUM_VALID = 1000
+
+        dialogs = get_dialogs(FULL_PATH)
+        num_dialogs = len(dialogs)
+
+        w2i = build_w2i(dialogs)
+        bow = build_bow(dialogs, w2i)
+
+        score_matrix = preprocess_utils.compute_score_matrix(bow, bow)
+        score_matrix[range(num_dialogs), range(num_dialogs)] = 0
+
+        overlap_values, max_overlap_indices = score_matrix.max(dim=1)
+        # increasing overlap
+        sorted_overlap_values, sorted_indices = overlap_values.sort()
+
+        sorted_scores = []
+        sorted_dialogs = []
+        for idx in sorted_indices:
+            sorted_scores.append(float(overlap_values[idx]))
+            sorted_dialogs.append(dialogs[idx])
+
+        test_dialogs = sorted_dialogs[:NUM_TEST]
+        valid_dialogs = sorted_dialogs[NUM_TEST:NUM_TEST+NUM_VALID]
+        train_dialogs = sorted_dialogs[NUM_TEST+NUM_VALID:]
+
+        splits = {
+            'test': test_dialogs,
+            'valid': valid_dialogs,
+            'train': train_dialogs
+        }
+
+        for split_name, split_dialogs in splits.items():
+            with open('{}.txt'.format(split_name), mode='w') as f:
+                f.writelines(split_dialogs)
+                total_turns = 0
+                num_dialogs = 0
+                for dialog in split_dialogs:
+                    num_dialogs += 1
+                    total_turns += dialog.count('__eou__')
+                print('{}: {} turns per dialog'.format(split_name, total_turns / num_dialogs))
+
+        print('Splitting finished!')
