5c97575ef710557f0d550c2d67d0747ede1760b6
Yuqiao Wen <yuqiao@ualbeta.ca>
2021-11-21 07:21:34 +0800

    Add threshold to remove exact duplicates

--- preprocessing/preprocess_utils.py
+++ preprocessing/preprocess_utils.py
@@ -307,7 +307,7 @@ def dump_results(train_df, eval_df, scores, max_overlap_indices, output_name):
 
     draw_scores(scores, prefix=output_name)
 
-    for threshold in [0.00, 0.25, 0.50, 0.60, 0.75, 1.00]:
+    for threshold in [0.00, 0.25, 0.50, 0.60, 0.75, 0.99, 1.00]:
         drop_indices = (scores > threshold).nonzero()[0]
         print('[{}]: Dropping {} samples for scores>{}'.format(
             output_name,
