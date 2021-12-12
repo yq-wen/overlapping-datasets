3df612f154f9c1d5bf194c38b2a7c98b9d5b2d00
Yuqiao Wen <yuqiao@ualbeta.ca>
2021-12-12 12:23:08 +0800

    Dump single turn dataset

--- preprocessing/dd/remove_exacts.py
+++ preprocessing/dd/remove_exacts.py
@@ -12,21 +12,37 @@ if __name__ == '__main__':
     parser.add_argument('--ref-path')
     parser.add_argument('--inp-path')
     parser.add_argument('--out-path')
+    parser.add_argument('--self', action='store_true')
 
     args = parser.parse_args()
 
-    ref_df = pd.read_csv(args.ref_path)
     inp_df = pd.read_csv(args.inp_path)
 
     seen = set()
     drop_indices = []
 
-    for idx, row in inp_df.iterrows():
-        text = preprocess_utils.preprocess_str(row['context'] + ' ' + row['response'])
-        if text not in seen:
-            seen.add(text)
-        else:
-            drop_indices.append(idx)
+    if args.self:
+
+        for idx, row in inp_df.iterrows():
+            text = preprocess_utils.preprocess_str(row['context'] + ' ' + row['response'])
+            if text not in seen:
+                seen.add(text)
+            else:
+                drop_indices.append(idx)
+
+    else:
+
+        ref_df = pd.read_csv(args.ref_path)
+
+        for idx, row in ref_df.iterrows():
+            text = preprocess_utils.preprocess_str(row['context'] + ' ' + row['response'])
+            if text not in seen:
+                seen.add(text)
+
+        for idx, row in inp_df.iterrows():
+            text = preprocess_utils.preprocess_str(row['context'] + ' ' + row['response'])
+            if text in seen:
+                drop_indices.append(idx)
 
     inp_df.drop(drop_indices).to_csv(args.out_path, index=False)
 
